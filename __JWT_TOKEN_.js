3adccbc783f2619a77fb4d9157ddc12cf31cbc88
Abrarulctg <jubairctg99@gmail.com>
2024-05-08 16:18:48 +0600

    started module 61 with JWT for car Doctor Server

---
+++ __JWT_TOKEN_.js
@@ -0,0 +1,14 @@
+// Make api Secure
+// The person who should have
+
+// Concept:
+// 1. Assign two token for each person (access TokenExpiredError, REfresh Token)
+// 2. token contains: user indentification (email, role, etc) valid for shorter duration
+// 3. refresh token is used: to recreate an access token that was expired.
+// 4. if refresh is invalid then logout the user.
+
+
+/**
+ * 1. jwt : JSON Web Token
+ * 2. 
+ */
